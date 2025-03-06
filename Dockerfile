e2479ac9a8396280101df8a4d9fb6132aea107fe
Elias Bistry <ebistry@mailbox.org>
2025-03-06 02:16:03 +0100

    install prisma instead of coping it

--- Dockerfile
+++ Dockerfile
@@ -57,4 +57,4 @@ ENV PORT=3000
 # https://nextjs.org/docs/pages/api-reference/config/next-config-js/output
 ENV HOSTNAME="0.0.0.0"
 
-CMD ["sh", "-c", "yarn prisma migrate deploy && node server.js"]
+CMD ["sh", "-c", "yarn add prisma && prisma migrate deploy && node server.js"]
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
-    outputFileTracingIncludes: {
-        "/": ["./node_modules/prisma/**/*", "./node_modules/@prisma/**/ *", "./node_modules/.bin/prisma"]
-    }
 };
 
 export default nextConfig;
